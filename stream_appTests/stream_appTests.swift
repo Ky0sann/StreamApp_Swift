a850209f9ce595deabc120c37e1e00d7ce2957fc
Alexandre-G-git <148047039+Alexandre-git-SDV@users.noreply.github.com>
2026-02-06 09:33:34 +0100

    New Tests on development branch
    New Tests on development branch

---
+++ stream_appTests/stream_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  stream_appTests.swift
+//  stream_appTests
+//
+//  Created by Cours on 06/02/2026.
+//
+
+import Testing
+@testable import stream_app
+
+struct stream_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
